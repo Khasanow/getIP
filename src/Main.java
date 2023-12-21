71259aab754e3fee63b67477234617d4fcadac5b
mikhail <hasanmishaalex@gmail.com>
2023-12-21 16:13:54 +0300

    upd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InetAddress google = null;
+        try{
+            google = InetAddress.getByName("www.google.com");
+        } catch (UnknownHostException e){
+            System.out.println("error "+e);
+        }
+        System.out.println("ip-address: "+google);
     }
 }
